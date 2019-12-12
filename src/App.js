aa2fe89f1622752e447ee7565c3167b5dee62a2a
Jelena Andjelkovic <jelena.andjelkovic@gmail.com>
2019-12-12 22:38:25 +0100

    phonebook window pop up

--- src/App.js
+++ src/App.js
@@ -13,11 +13,17 @@ function App() {
     tmp.push({name,phone})
     setPhoneL(tmp);
   }
+  // const clickDelete=(index,1)=>{
+  //   let tmp=[...phoneL];
+  //   tmp.splice({index,1});
+  //   setPhoneL(tmp);
+
+  // }
 
 
   return (
    <div>
-     <Form clickHandler={clickHandler} phoneL={phoneL}></Form>
+     <Form clickHandler={clickHandler} phoneL={phoneL} setPhoneL={setPhoneL}></Form>
      <PhoneList phoneL={phoneL}></PhoneList>
 
    </div>
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,39 +1,50 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-const Form=(props)=>{
-    // let [phoneL]=props;
+const Form = (props) => {
+    let phoneL = props.phoneL;
+    let setPhoneL=props.phoneL;
     const [newName, setNewName] = useState('');
     const [newPhone, setNewPhone] = useState('');
-    let {clickHandler}=props;
+    let clickHandler = props.clickHandler;
+    // let clickDelete=props.clickDelete;
 
-const handleInput= (e) =>{
-    e.preventDefault();
-    console.log(`name${newName}${newPhone}`);
-    clickHandler(newName, newPhone);
-}
+    const handleInput = (e) => {
+        // e.preventDefault();
+        // console.log(`name${newName}${newPhone}`);
+        // clickHandler(newName, newPhone);
+        let tmp = [...phoneL]
+        console.log(tmp)
+        tmp = tmp.filter(x => newName === x.name);
+        console.log(tmp)
 
-const onNameChange= (e) =>{
-    
-    setNewName(e.target.value);
-}
+        if (tmp.length > 0) {
+            // alert('Pitanje');
+            const r = window.confirm('Do you want to change the phone number');
+            if (r === true) {
+                
+                // setPhoneL('');
+                clickHandler(newName, newPhone);
+            }
+        } else {
+            console.log(`name${newName}${newPhone}`);
+            clickHandler(newName, newPhone);
+            // alert('Ne postoji, dodajemo');
+        }
+        e.preventDefault();
+    }
 
+    const onNameChange = (e) => {
+        setNewName(e.target.value);
+    }
 
+    const onPhoneChange = (e) => {
+        setNewPhone(e.target.value);
+    }
 
-
-const onPhoneChange= (e) =>{
-    setNewPhone(e.target.value);
-}
-
-// clickHandler=(e)=>{
-//     // setPhoneL('')
-// }
-
-
-
-    return(
+    return (
         <form >
-            <input type='text' id='name' onChange={onNameChange}/>
-            <input type='text' id='phone' onChange={onPhoneChange}/>
+            <input type='text' id='name' onChange={onNameChange} />
+            <input type='text' id='phone' onChange={onPhoneChange} />
             <button type='submit' onClick={handleInput}>SUBMIT</button>
         </form>
     );
