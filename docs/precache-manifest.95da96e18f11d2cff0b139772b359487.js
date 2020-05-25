f1cacdd8de2ac8ac6eaf4ba2fddc8756762db719
jelenaandj <jelena.andjelkovic453@gmail.com>
2020-05-25 14:34:42 +0200

    build

---
+++ docs/precache-manifest.95da96e18f11d2cff0b139772b359487.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "367f07dd48c2afda89937cc1cc580278",
+    "url": "/phonebook/index.html"
+  },
+  {
+    "revision": "a05814b8b21c43bdc27e",
+    "url": "/phonebook/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "07b5fd25aef645d022f5",
+    "url": "/phonebook/static/js/2.096ab65e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/phonebook/static/js/2.096ab65e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a05814b8b21c43bdc27e",
+    "url": "/phonebook/static/js/main.bb8eb9b7.chunk.js"
+  },
+  {
+    "revision": "1024267a5a36cad40687",
+    "url": "/phonebook/static/js/runtime-main.befcf53f.js"
+  }
+]);
